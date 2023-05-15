c92d3c9f5aa474dac5fa489ab09b4b04ac724caf
PepeBiadayoli <josebiadayoli@gmail.com>
2023-05-15 16:11:32 -0400

    frist commit

---
+++ src/controllers/whatsappControllers.js
@@ -0,0 +1,13 @@
+const VerifyToken = (req, res) => {
+    res.send("Hola verifyToken")
+}
+
+const ReceivedMessage = (req, res) => {
+    res.send("Hola Received")
+}
+
+
+module.exports = {
+    VerifyToken,
+    ReceivedMessage
+}
